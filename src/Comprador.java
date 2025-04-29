501cf37bcf0a84c3d7bdaedfd147a7b4d9d17b78
byNoRx <norxgamer12@gmail.com>
2025-04-29 14:37:02 -0400

    Actualizar documentación y añadir throws al constructor

--- src/Comprador.java
+++ src/Comprador.java
@@ -1,8 +1,20 @@
+/**
+ * Clase Comprador que representa un cliente que compra productos usando una máquina expendedora.
+ */
 public class Comprador {
     private final String sonido;
     private final int vuelto;
 
-    public Comprador(Moneda moneda, int cualBebida, Expendedor expendedor) {
+    /**
+     * Constructor de Comprador.
+     * @param moneda la moneda con la que se va a realizar la compra
+     * @param cualBebida el número del producto a comprar
+     * @param expendedor la máquina expendedora en la que se va a comprar
+     * @throws NoHayProductoException si no hay producto disponible
+     * @throws PagoInsuficienteException si el pago es insuficiente
+     * @throws PagoIncorrectoException si el pago es incorrecto
+     */
+    public Comprador(Moneda moneda, int cualBebida, Expendedor expendedor) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         Producto bebida_comprada = expendedor.comprarProducto(moneda, cualBebida);
         if (bebida_comprada != null) {
             this.sonido = bebida_comprada.getSabor();
@@ -18,10 +30,16 @@ public class Comprador {
         this.vuelto = sum_vuelto;
     }
 
+    /**
+     * @return el vuelto que se ha obtenido al hacer la compra
+     */
     public int cuantoVuelto () {
         return vuelto;
     }
 
+    /**
+     * @return el sonido del producto que se ha comprado
+     */
     public String queBebiste () {
         return sonido;
     }
