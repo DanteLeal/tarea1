d8cf5c4a82e0830e8cb3a75fa49a2150a857f4fb
byNoRx <norxgamer12@gmail.com>
2025-05-02 15:32:22 -0400

    Remover variable y función que no se usa

--- src/Producto.java
+++ src/Producto.java
@@ -1,17 +1,11 @@
 public abstract class Producto {
 
     // Variables
-    private int precio;
     private final int serie;
 
     // Constructor method
     public Producto(int serie) {
         this.serie = serie;
-    }
-
-    // Setter method
-    public void setPrecio(int precio) {
-        this.precio = precio;
     }
 
     // Getter method
