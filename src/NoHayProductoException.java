268c4ae109aebf8cf4bbab36b2bc3843ff3bb077
byNoRx <norxgamer12@gmail.com>
2025-05-02 20:19:45 -0400

    Actualizar documentación

--- src/NoHayProductoException.java
+++ src/NoHayProductoException.java
@@ -1,4 +1,12 @@
+/* 
+ * Clase NoHayProductoException que representa un error en la compra de un producto en una maquina expendedora.
+ */
 public class NoHayProductoException extends Exception {
+    /**
+     * Constructor de la clase NoHayProductoException.
+     *
+     * @param message el mensaje de error.
+     */
     public NoHayProductoException(String message) {
         super(message);
     }
