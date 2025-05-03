667f7ef06b0673ce478851d14a35d0bcca704be4
byNoRx <norxgamer12@gmail.com>
2025-05-02 20:00:43 -0400

    Actualizar documentación

--- src/Bebida.java
+++ src/Bebida.java
@@ -1,52 +1,88 @@
-// Clase Bebida (Bebidas que se almacenarán en Depósitos de la clase Deposito.java)
+/**
+ * Clase Bebida que representa una bebida que se puede comprar en una maquina expendedora.
+ */
 public abstract class Bebida extends Producto {
 
-    // Constructor method
+    /**
+     * Metodo constructor
+     * 
+     * @param serie la serie de la bebida
+     */
     public Bebida(int serie) {
         super(serie);
     }
 
-    // Abstract methods
+    /**
+     * Metodo abstracto que retorna el sabor de la bebida
+     */
     public abstract String getSabor();
 }
 
-// Subclase de Bebida: CocaCola (Al ser subclase, hereda las variables de Bebida)
+/**
+ * Subclase de Bebida: CocaCola (Al ser subclase, hereda las variables de Bebida)
+ */
 class CocaCola extends Bebida {
-    // Constructor method
+    /**
+     * Metodo constructor
+     * @param serie la serie de la bebida     
+     */
     public CocaCola(int serie) {
         super(serie);
     }
 
-    // Abstract method from superclass
+    /** 
+     * Metodo abstracto que retorna el sabor de la bebida
+     * 
+     * @return el sabor de la bebida
+     */
     @Override
     public String getSabor() {
         return "cocacola";
     }
 }
 
-// Subclase de Bebida: Fanta (Al ser subclase, hereda las variables de Bebida)
+/**
+ * Subclase de Bebida: Fanta (Al ser subclase, hereda las variables de Bebida)
+ */
 class Fanta extends Bebida {
-    // Constructor method
+    /** 
+     * Metodo constructor
+     * 
+     * @param serie la serie de la bebida
+     */
     public Fanta(int serie) {
         super(serie);
     }
 
-    // Abstract method from superclass
+    /**
+     * Metodo abstracto que retorna el sabor de la bebida
+     * 
+     * @return el sabor de la bebida
+     */
     @Override
     public String getSabor() {
         return "fanta";
     }
 }
 
-
-// Subclase de Bebida: Sprite (Al ser subclase, hereda las variables de Bebida)
+/** 
+ * Subclase de Bebida: Sprite (Al ser subclase, hereda las variables de Bebida)
+ */
 class Sprite extends Bebida {
-    // Constructor method
+    /**
+     * Metodo constructor
+     *  
+     * @param serie la serie de la bebida
+     */
     public Sprite(int serie) {
         super(serie);
     }
 
-    // Abstract method from superclass
+    /**
+     * Metodo abstracto que retorna el sabor de la bebida
+     * 
+     * @return el sabor de la bebida
+     */
     @Override
     public String getSabor() {
         return "sprite";
