1125ecdcfc09d13f85c0131412958d00737b2a3d
byNoRx <norxgamer12@gmail.com>
2025-05-02 21:28:04 -0400

    Actualizar documentación

--- src/ProductoTipo.java
+++ src/ProductoTipo.java
@@ -1,3 +1,6 @@
+/** 
+ * Enum con los tipos de productos
+ */
 public enum ProductoTipo {
     COCA(1, 1100),
     FANTA(2, 1200),
@@ -8,15 +11,31 @@ public enum ProductoTipo {
     private final int numero;
     private final int precio;
 
+    /**
+     * Constructor
+     * 
+     * @param numero numero del producto
+     * @param precio precio del producto
+     */
     ProductoTipo(int numero, int precio) {
         this.numero = numero;
         this.precio = precio;
     }
 
+    /**
+     * Obtiene el numero del producto
+     * 
+     * @return el numero del producto
+     */
     public int getNumero() {
         return numero;
     }
 
+    /**
+     * Obtiene el precio del producto
+     * 
+     * @return el precio del producto
+     */
     public int getPrecio() {
         return precio;
     }
